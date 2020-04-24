b367e6e040532441584b70a373aded3033f8f0f7
Ihor Lavro <i.lavro@plarium.com>
2020-04-24 19:45:01 +0300

    webpack

---
+++ build-sandbox/webpack.config.js
@@ -0,0 +1,92 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = (env = {}) => {
+  const { mode = 'development' } = env;
+
+  const isProd = mode === 'production';
+  const isDev = mode === 'development';
+
+  const getStyleLoaders = () => {
+    return [
+      isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+      'css-loader'
+    ]
+  }
+
+  const getPlugins = () => {
+    const plugins = [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        title: 'Hello, world!',
+        buildTime: new Date().toString(),
+        template: 'public/index.html'
+      })
+    ];
+
+    if (isProd) {
+      plugins.push(new MiniCssExtractPlugin({
+        filename: 'main-[hash:8].css'
+      }))
+    }
+
+    return plugins;
+  }
+
+  return {
+    mode: isProd ? 'production' : isDev && 'development',
+
+    output: {
+      filename: isProd ? 'main-[hash:8].js' : undefined
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.js/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.(css)$/,
+          use: getStyleLoaders()
+        },
+        {
+          test: /\.(s[ca]ss)$/,
+          use: [...getStyleLoaders(), 'sass-loader']
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|svg|ico)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'images',
+                name: '[name]-[sha1:hash:7].[ext]'
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(ttf|otf|eot|woff|woff2)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'fonts',
+                name: '[name].[ext]'
+              }
+            }
+          ]
+        }
+      ]
+    },
+
+    plugins: getPlugins(),
+
+    devServer: {
+      open: true
+    }
+  }
+};
